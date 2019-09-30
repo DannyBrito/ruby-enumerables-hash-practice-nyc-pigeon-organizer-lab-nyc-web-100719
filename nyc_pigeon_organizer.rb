73ea1c062fbbe5b23b79ef783fdaeb6523bd6045
Danny Brito <britodanny1997@gmail.com>
2019-09-30 17:11:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data ={}
+  data.each{|char, ind|
+   ind.each{|adj, item_r|
+   # puts "#{char}: #{adj} - #{item_r}"
+    item_r.each{|ind_item|
+   # puts "ind_item: #{ind_item}"
+    if new_data[ind_item] == nil
+      new_data[ind_item] = {char => [adj.to_s]}
+    else 
+        if new_data[ind_item][char] == nil
+        new_data[ind_item][char] = [adj.to_s]
+        else
+        new_data[ind_item][char] << adj.to_s
+        end 
+    end
+    }
+   }
+  }
+  new_data
 end
